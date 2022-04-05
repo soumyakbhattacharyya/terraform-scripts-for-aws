00779582167f1aa639f1cebc3ff32846e500caf7
soumyak <bhattacharyya.soumyak@gmail.com>
2022-04-05 11:41:28 +0530

    updated the load balancer module's version to latest value

--- topology/c10-02-ELB-classic-loadbalancer.tf
+++ topology/c10-02-ELB-classic-loadbalancer.tf
@@ -1,14 +1,22 @@
 # Terraform AWS Classic Load Balancer (ELB-CLB)
 module "elb" {
   source  = "terraform-aws-modules/elb/aws"
-  version = "2.5.0"
+  version = "3.0.1"
   name = "${local.name}-myelb"
+
+  # load balancer belongs to public subnets
   subnets         = [
     module.vpc.public_subnets[0],
     module.vpc.public_subnets[1]
   ]
+
+  # refer to the security group dedicated for the load balancer
   security_groups = [module.loadbalancer_sg.security_group_id]
+
+  # internal = false signifies that it is an external facing load balancer
   #internal        = false
+
+  # there are 2 listener configurations at port 80 and 81 
 
   listener = [
     {
@@ -33,7 +41,7 @@ module "elb" {
     timeout             = 5
   }
 
-  # ELB attachments
+  # intances where the load balancer is going to send the traffic
   number_of_instances = var.private_instance_count
   instances           = [
     element([for instance in module.ec2_private: instance.id], 0),
--- topology/c5-05-securitygroup-loadbalancersg.tf
+++ topology/c5-05-securitygroup-loadbalancersg.tf
@@ -6,9 +6,11 @@ module "loadbalancer_sg" {
   name = "loadbalancer-sg"
   description = "Security Group with HTTP open for entire Internet (IPv4 CIDR), egress ports are all world open"
   vpc_id = module.vpc.vpc_id
+  
   # Ingress Rules & CIDR Blocks
   ingress_rules = ["http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
+  
   # Egress Rule - all-all open
   egress_rules = ["all-all"]
   tags = local.common_tags
