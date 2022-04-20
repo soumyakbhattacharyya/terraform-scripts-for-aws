a6dc75a9216db20a39f2b3852838e6b2c53ab4db
soumyak <bhattacharyya.soumyak@gmail.com>
2022-04-20 15:05:34 +0530

    renamed version files

--- topology/infra-v1-1-versions.tf
+++ topology/infra-v1-1-versions.tf
@@ -1,15 +1,15 @@
 # Terraform Block
 terraform {
-  required_version = ">= 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_version = ">= 0.14" # which means any version equal & above 1.1.7
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.8"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "~> 3.0"
-    }        
+    }
   }
 }
 
